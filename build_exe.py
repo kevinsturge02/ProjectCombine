82a4dca85b668ab70326295ccf4aeae45e24c677
kevin sturge <crazyguy644@gmail.com>
2025-02-15 17:04:26 -0500

    removes false positive antivirus flag

--- build_exe.py
+++ build_exe.py
@@ -1,12 +1,10 @@
-import PyInstaller.__main__
-
-# PyInstaller configuration
 PyInstaller.__main__.run([
-    'ZipExtractorGUI.py',               # Your main script
-    '--name=ProjectCombine',             # Name of the executable
-    '--onefile',                        # Create a single executable file
-    '--windowed',                       # Don't show console window
-    '--icon=pc.ico',                    # Your custom icon
-    '--clean',                          # Clean cache before building
-    '--noconfirm',                      # Replace output directory without asking
+    'ZipExtractorGUI.py',
+    '--name=CodeExtractor',
+    '--onefile',
+    '--windowed',
+    '--icon=pc.ico',
+    '--clean',
+    '--noconfirm',
+    '--noupx'  # Add this flag to avoid compression that triggers some antivirus
 ])
